13bdffef518be0a7ce0d5ea84e8340ed0eb0e258
ssoniashahh <119973580+ssoniashahh@users.noreply.github.com>
2022-12-08 18:19:25 -0500

    Update connect_sqlite.py

--- connect_sqlite.py
+++ connect_sqlite.py
@@ -86,12 +86,42 @@ clinics = [(11, 'Lake Highland', '653 S Ventura Avenue', '1112223333'),
            (15, 'AdventHealth', '7678 Yeehaw Junction', '5556667777')]
 cursor.executemany('INSERT INTO Clinic VALUES(?,?,?,?);', clinics)
 
+query = """
+    SELECT *
+    FROM Clinic
+    """
+cursor.execute(query)
+# Extract column names from cursor
+column_names = [row[0] for row in cursor.description]
+# Fetch data and load into a pandas dataframe
+table_data = cursor.fetchall()
+df = pd.DataFrame(table_data, columns=column_names)
+# Examine dataframe
+print(df)
+print(df.columns)
+print(' ')
+
 staff = [(16, 'Bart', '6655 W Manor Drive', '9875726490', '1987-10-23', 'Manager', 10000, 11),
          (18, 'Isa', '923 Sugarplum Trail', '9265820271', '1998-12-12', 'Vet', 20000, 11),
          (21, 'Gerald', '8913 Phillips Avenue', '5679209976', '1994-12-24', 'Vet', 30000, 11),
          (23, 'Vanessa', '9251 Point Rowe Drive', '8764561234', '2001-8-12', 'Nurse', 40000, 11),
          (17, 'Veronica', '10713 Factorial Park Drive', '9098874444', '1994-2-12', 'Receptionist', 50000, 11)]
 cursor.executemany('INSERT INTO Staff VALUES(?,?,?,?,?,?,?,?)', staff)
+
+query = """
+    SELECT *
+    FROM Staff
+    """
+cursor.execute(query)
+# Extract column names from cursor
+column_names = [row[0] for row in cursor.description]
+# Fetch data and load into a pandas dataframe
+table_data = cursor.fetchall()
+df = pd.DataFrame(table_data, columns=column_names)
+# Examine dataframe
+print(df)
+print(df.columns)
+print(' ')
 
 owners = [(1, 'Martha', '123 SW Diane Avenue', '9352340000', 11),
          (2, 'Ron', '1251 N 7th Street', '1234567890', 11),
@@ -100,6 +130,21 @@ owners = [(1, 'Martha', '123 SW Diane Avenue', '9352340000', 11),
          (5, 'Victor', '1635 N 3rd Street', '6665559922', 11)]
 cursor.executemany('INSERT INTO Owner VALUES(?,?,?,?,?)', owners)
 
+query = """
+    SELECT *
+    FROM Owner
+    """
+cursor.execute(query)
+# Extract column names from cursor
+column_names = [row[0] for row in cursor.description]
+# Fetch data and load into a pandas dataframe
+table_data = cursor.fetchall()
+df = pd.DataFrame(table_data, columns=column_names)
+# Examine dataframe
+print(df)
+print(df.columns)
+print(' ')
+
 pets = [(6, 'Magic', '2012-11-7', 'Dog', 'Bichon', 'White', 2, 11),
         (7, 'Siddhu', '2017-12-11', 'Dog', 'Bichon', 'White', 5, 11),
         (8, 'Hari', '2020-10-19', 'Dog', 'Golden Doodle', 'White', 3, 11),
@@ -107,12 +152,42 @@ pets = [(6, 'Magic', '2012-11-7', 'Dog', 'Bichon', 'White', 2, 11),
         (10, 'Aarav', '2003-11-4', 'Snake', 'Python', 'Red', 1, 11)]
 cursor.executemany('INSERT INTO Pet VALUES(?,?,?,?,?,?,?,?);', pets)
 
+query = """
+    SELECT *
+    FROM Pet
+    """
+cursor.execute(query)
+# Extract column names from cursor
+column_names = [row[0] for row in cursor.description]
+# Fetch data and load into a pandas dataframe
+table_data = cursor.fetchall()
+df = pd.DataFrame(table_data, columns=column_names)
+# Examine dataframe
+print(df)
+print(df.columns)
+print(' ')
+
 exams = [(1, 'Broken Bone', 'Leg is broken', '2022-12-6', 'Adjust bone', 6, 18),
          (2, 'Allergies', 'Allergies bad when outside', '2022-12-6', 'Medication', 7, 21),
          (3, 'Broken Bone', 'Leg is broken', '2022-12-6', 'Adjust bone', 8, 18),
          (4, 'Rash', 'Rash on hip', '2022-12-6', 'Ointment Cream', 9, 21),
          (5, 'Tooth broken', 'Upper tooth is broken', '2022-12-6', 'Medication', 10, 18)]
 cursor.executemany('INSERT INTO Examination VALUES(?,?,?,?,?,?,?);', exams)
+
+query = """
+    SELECT *
+    FROM Examination
+    """
+cursor.execute(query)
+# Extract column names from cursor
+column_names = [row[0] for row in cursor.description]
+# Fetch data and load into a pandas dataframe
+table_data = cursor.fetchall()
+df = pd.DataFrame(table_data, columns=column_names)
+# Examine dataframe
+print(df)
+print(df.columns)
+print(' ')
 
 # Select data
 query = """
